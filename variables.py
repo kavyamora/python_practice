4fdb53b6294b46414d8768947a670d26ef8c25fc
kavya mora <kavyamora4@gmail.com>
2026-08-11 18:43:11 +0530

    Add examples and rules for variable assignment
    Add variable assignment examples and rules in variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+# one value is assign to one variable 
+# a=1
+# print(a)
+
+# multiple values asign to the single variable
+# a=1,2,3
+# print(a)
+
+# single value assign to multiple variables 
+# a,b,c=1
+# print(a,b,c)
+
+#rules
+#1.alphabtes and undescore
+#2.cannot start with digit or symbol
+
+abc4343=3
+print(id(abc4343)) 
