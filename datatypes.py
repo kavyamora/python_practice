09a7bd69c489e45bf5baf4a7a17246fc05de44dc
kavya mora <kavyamora4@gmail.com>
2026-08-11 18:46:51 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+# '''
+# int 
+# float
+# boolean
+# complex
+# '''
+
+# a=1
+# print(type(a))
+
+# a=True
+# b=False
+# print(type(a),type(b))
+
+a=2
+print(float(a))
+
+# v=2+5j
+# print(type(v))
+
+b=5.5
+print(int(b))
+
+'''
+data lose--> explicit type conversion
+no data lose--->implicit type conversion
+'''
