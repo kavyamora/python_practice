60aff37408499d28ef995b4205e224e7c34783b8
kavya mora <kavyamora4@gmail.com>
2026-08-11 18:47:48 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,24 @@
+# '''
+# arithmetic
+# logical
+# comparison
+# '''
+
+# '''
+# arithmetic operaters
+# '''
+# a=1
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+# print(a>b)
+# print(a==b)
+
+age=20
+print(age>18 and age<30)
