994215870c1664917b7a8f71313a000aeee8daa5
kavya mora <kavyamora4@gmail.com>
2026-08-13 20:35:59 +0530

    Add looping statements examples and syntax
    Added examples and syntax for for and while loops.

---
+++ looping statements.py
@@ -0,0 +1,30 @@
+'''
+looping statemts part of control statements
+   for loop
+     while loop
+'''
+
+'''
+ for loop 
+ syntax:
+     for i in range(0,10)
+     print(i)
+'''
+
+# a=[1,12,14,15,20]
+# for i in a:
+#     print(i)
+  
+'''
+while loop 
+syntax:
+   while TRUE:
+      print("Hi")
+           this will be continuous until iit become false:
+'''
+
+count = 1
+
+while count <= 3:
+    print(count)
+    count += 1
