e8f2711f9de0168b281b23a0a219dcde364187a5
kavya mora <kavyamora4@gmail.com>
2026-08-18 19:23:11 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,1 +1,48 @@
+'''
+collect of characters
+'''
+'''
+represented in
+         single('')
+         double("")
+         triple(''' ''')
+'''
+'''
+methods
+1.lower()
+2.upper()
+3.endswith()
+4.startswith()
+5.replace()
+6.count()
+7.Rstrip()
+8.lstrip()
+9.strip()
+10.remove suffix()
+11.remove prefix()
+12.index()
+13.find()
+'''
 
+# a='python life'
+# b="python life"
+# c=''' python life'''
+# print(type(a),type(b),type(c))
+
+
+''' lower()'''
+# a="pythonlife program"
+# print(a.upper())
+
+''' upper()'''
+# a="python life"
+# print(a.lower())
+
+''' ends with()'''
+# v="python life"
+# print(v.endswith("life"))
+
+''' count()'''
+v="python life"
+print(v.count("python life"))
+print(v.count("python""life"))
