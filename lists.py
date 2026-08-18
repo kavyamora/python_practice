032b62af0d30c11de8bf1c399aeb6d87a33c4b65
kavya mora <kavyamora4@gmail.com>
2026-08-18 19:35:30 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+'''
+list 
+ 1.represented in []
+ 2.mutable data type(change anywhere)
+ 3.allow duplicates
+ 4.allow indexing
+ '''
+#v=[]
+# print(type(v))
+
+v=[1,12,13,14,15,25,'kiran']
+print(type(v))
+print(v[2])
+print(v[-3])
+print(v[0:6:2])
+print(v[2:4:1])
+print(v[2:-4:1])
